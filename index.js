2268ad4e7aafb2d45ce7c776ed02de8b840c1bab
Olaf Mihari <olafnjenga@gmail.com>
2023-03-12 05:01:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+const currentUser = 'Mihari';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let  excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
